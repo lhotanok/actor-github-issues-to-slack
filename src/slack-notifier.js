4ed37df072547e9412338c13ac94729becd69033
Kristýna Lhoťanová <kristyna.lhotanova@gmail.com>
2021-11-01 19:02:47 +0100

    Implement slack notification using block elements

--- src/slack-notifier.js
+++ src/slack-notifier.js
@@ -4,37 +4,47 @@ const { SLACK_ACTOR_ID, OPENED_ISSUE, CLOSED_ISSUE } = require('./constants');
 const { utils: { log } } = Apify;
 
 exports.sendModifiedIssuesNotification = async (modifiedIssues, { channel, token }, { openedIssues, closedIssues }) => {
-    const text = buildNotificationMessage(modifiedIssues, { openedIssues, closedIssues });
+    const text = 'Github issues tracker';
+    const blocks = buildNotificationBlocks(modifiedIssues, { openedIssues, closedIssues });
 
     const slackActorInput = {
         token,
         channel,
         text,
+        blocks,
     };
 
     const apifyClient = Apify.newClient({ token: process.env.APIFY_TOKEN });
     const actorClient = apifyClient.actor(SLACK_ACTOR_ID);
 
-    // const run = await actorClient.call(slackActorInput);
+    const run = await actorClient.call(slackActorInput);
 
     log.info(`Slack notification:
     ${JSON.stringify(slackActorInput, null, 2)}`);
 };
 
-function buildNotificationMessage(modifiedIssues, { openedIssues, closedIssues }) {
-    let message = '';
+function buildNotificationBlocks(modifiedIssues, { openedIssues, closedIssues }) {
+    const blocks = [];
+    log.info(`OPENED ISSUES: ${openedIssues}, CLOSED ISSUES: ${closedIssues}`);
 
-    if (openedIssues) {
+    if (openedIssues !== false) {
         const opened = getIssuesWithState(modifiedIssues, OPENED_ISSUE);
-        message = `${buildIssuesNotification(opened, 'opened')}\n`;
+        blocks.push(buildHeaderBlock(`Newly opened issues:`));
+        blocks.push(...buildIssuesNotificationBlocks(opened));
     }
 
-    if (closedIssues) {
+    if (closedIssues !== false) {
         const closed = getIssuesWithState(modifiedIssues, CLOSED_ISSUE);
-        message = `${message}${buildIssuesNotification(closed, 'closed')}`;
+
+        if (blocks.length !== 0) {
+            blocks.push({ type: 'divider' });
+        }
+
+        blocks.push(buildHeaderBlock(`Newly closed issues:`));
+        blocks.push(...buildIssuesNotificationBlocks(closed));
     }
 
-    return message;
+    return blocks;
 }
 
 function getIssuesWithState(modifiedIssues, state) {
@@ -52,19 +62,79 @@ function getIssuesWithState(modifiedIssues, state) {
     return issues;
 }
 
-function buildIssuesNotification(issues, state) {
-    let notification = `Newly ${state} issues:`;
+function buildHeaderBlock(text) {
+    return {
+        type: 'header',
+        text: {
+            type: 'plain_text',
+            text,
+        },
+    };
+}
+
+function buildIssuesNotificationBlocks(issues) {
+    const notificationBlocks = [];
 
     Object.keys(issues).forEach((repository) => {
         const repositoryIssues = issues[repository];
 
         if (repositoryIssues.length !== 0) {
-            notification = `${notification}\nRepository ${repository}:`;
             repositoryIssues.forEach((issue) => {
-                notification = `${notification}\n${JSON.stringify(issue, null, 2)}`;
+                const notificationBlock = buildIssueNotificationBlock(issue, repository);
+                notificationBlocks.push(notificationBlock);
             });
         }
     });
 
-    return notification;
+    return notificationBlocks;
+}
+
+function buildIssueNotificationBlock(issue, repository) {
+    const { title, state, labels, author, createdAt, updatedAt, assignee, comments, url } = issue;
+
+    return {
+        type: 'section',
+        text: {
+            type: 'mrkdwn',
+            text: `*${title}*`,
+        },
+        fields: [
+            {
+                type: 'mrkdwn',
+                text: `*Repository*: ${repository}`,
+            },
+            {
+                type: 'mrkdwn',
+                text: `*State*: ${state}`,
+            },
+            {
+                type: 'mrkdwn',
+                text: `*Labels*: ${JSON.stringify(labels)}`,
+            },
+            {
+                type: 'mrkdwn',
+                text: `*Author*: ${author}`,
+            },
+            {
+                type: 'mrkdwn',
+                text: `*Created at*: ${createdAt}`,
+            },
+            {
+                type: 'mrkdwn',
+                text: `*Updated at*: ${updatedAt}`,
+            },
+            {
+                type: 'mrkdwn',
+                text: `*Assignee*: ${assignee}`,
+            },
+            {
+                type: 'mrkdwn',
+                text: `*Comments*: ${comments}`,
+            },
+            {
+                type: 'mrkdwn',
+                text: `*Url*: ${url}`,
+            },
+        ],
+    };
 }
